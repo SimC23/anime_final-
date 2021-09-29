eec783e8c0890d02a30810f468c8dccc3e7347dd
simranjit charan <simran.s.charan@gmail.com>
2021-09-29 17:41:34 -0400

    sim commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import { Sidebar } from './components/Sidebar';
+import { Maincontent } from './components/Maincontent';
+
+let searchDelay = null;
+
+const App = () => {
+
+
+	const [animeList, setAnimeList] = useState([])
+	const [topAnime, setTopAnime] = useState([])
+	const [search, setSearch] = useState('')
+	const [loading,setLoading] = useState()
+	const [page,setPage] = useState(1)
+
+	useEffect(() => {
+		GetTopAnime();
+	}, [])
+
+	const GetTopAnime = async () => {
+		const temp = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`)
+			.then(res => res.json());
+		setTopAnime(temp.top.slice(0, 10))
+	}
+
+	const handleScroll = (event) => {
+		const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+
+		if ((scrollTop + clientHeight) >= (scrollHeight-100)) {
+			setPage(prev => prev + 1)
+			console.log(page)
+			
+		}
+	
+	}
+
+
+	const handleSearch = e => {
+		const searchValue = e.target.value;
+		
+		setSearch(searchValue)
+
+		clearTimeout(searchDelay);
+
+		if (searchValue) {
+      searchDelay = setTimeout(async () => {
+
+				fetch(`https://api.jikan.moe/v3/search/anime?q=${searchValue}&order_by=title&sort=asc&limit=20&page=1`)
+				.then(res => res.json())
+				.then(result => {
+
+					if (result.results) {
+						setAnimeList(result.results);
+						setSearch('')
+						setLoading(true)
+					}
+				});
+
+      }, 1000);
+    }
+
+	}
+
+
+	// const getSearch = e => {
+
+	// 	if (e.key === 'Enter') {
+	// 		fetch(`https://api.jikan.moe/v3/search/anime?q=${search}&order_by=title&sort=asc&limit=20`)
+	// 			.then(res => res.json())
+	// 			.then(result => {
+	// 				setAnimeList(result.results)
+	// 				setSearch('')
+	// 			})
+	// 	}
+	// }
+
+	return (
+		<div className="App">
+			<Header />
+			<div className='content-wrap' onScroll={handleScroll}>
+				<Maincontent handleSearch={handleSearch} search={search} animeList={animeList} loading={loading} setLoading={setLoading}/>
+				<Sidebar topAnime={topAnime} />
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Maincontent.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import AnimeCard from './AnimeCard'
+
+
+export const Maincontent = ({search, handleSearch, animeList, loading, setLoading}) => {
+    
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		setLoading(false)
+	}
+
+    return (
+        <main>
+        <div className='main-head'>
+        <form 
+					className="search-box"
+					onSubmit={handleSubmit}
+					>
+					<input 
+						type="search"
+						placeholder="Search for an anime..."
+						required
+						value={search}
+						onChange={handleSearch}
+						// onKeyPress={getSearch}
+					/>
+				</form>
+				</div>
+				<div className='main-head'>
+					{loading != false ? null: <span>Loading...</span>}
+				</div>
+				<div className="anime-list" >
+				{animeList.map(anime => (
+					<AnimeCard
+						anime={anime}
+						key={anime.mal_id} 
+						/>
+				))}
+			</div>
+        </main>
+    )
+}
